2d2f6ae83675e68db82e77e15a7027af933e5045
vedansh-01 <vedansh.24bcan0190@jecrcu.edu.in>
2026-07-01 16:10:10 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.68%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,16 @@
+    class Solution {
+public:
+    int rob(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1){
+            return arr[0];
+        }
+        vector<int> dp(n);
+        dp[0]=arr[0];
+        dp[1]=max(arr[0], arr[1]);
+        for(int i=2; i<n; i++){
+            dp[i]=max(arr[i]+dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
+    }
+};
