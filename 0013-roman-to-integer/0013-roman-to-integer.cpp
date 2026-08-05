d1e1f5918ef23d17ba741187eb89457af100284d
vedansh <194820266+vedansh-01@users.noreply.github.com>
2026-08-05 11:50:47 +0530

    Time: 2 ms (78.86%), Space: 13.3 MB (38.45%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -7,7 +7,7 @@ public:
         };
         
         int result = 0;
-        for (int i = 0; i < s.size(); ++i) {
+        for (int i = 0; i < s.size();i++) {
             int curr = roman[s[i]];
             int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
             
